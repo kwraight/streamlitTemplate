a40927c3fd827c746c67a5bd67efbd5c647d258c
wraight <kenneth.gibb.wraight@cern.ch>
2021-06-18 11:53:56 +0100

    correct typeO

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,4 +1,4 @@
-docker build . -f dockerFiles/Dockerfile -t kwraight/itk-pdb-template:1.0
+docker build . -f dockerFiles/Dockerfile -t kwraight/template-app:1.0
 docker push kwraight/template-app:1.0
-docker build . -f dockerFiles/Dockerfile -t kwraight/itk-pdb-template:latest
+docker build . -f dockerFiles/Dockerfile -t kwraight/template-app:latest
 docker push kwraight/template-app:latest
